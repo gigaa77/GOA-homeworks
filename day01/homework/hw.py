d3c74b92d0594ffa8da15ba6faa4e09335d1fa8d
gigaa77 <gogitidzegiga2009@gmail.com>
2025-10-01 21:13:54 +0400

    “commit”

---
+++ day01/homework/hw.py
@@ -0,0 +1,75 @@
+#1) print() ფუნქციის (რამოდენიმეჯერ) გამოყენებით, დაპრინტეთ სამკუთხედი, ნებისმიერი სიმბოლოთი, მაგალითად:
+print("#")
+print("##")
+print("###")
+print("####")
+print("#####")
+print("######")
+
+#2) გამოიტანეთ თქვენი საყვარელი 5 საჭმელი, ყველა თითო ხაზზე
+
+print("rom gaarchio")
+
+print("khinkali")
+print("lobiani")
+print("adjarian khachapuri")
+print("mwvadi")
+print("caesar salad")
+
+#3) მარტივი გამოიტანეთ Hello და თქვენი სახელი
+
+print("rom gaarchio")
+
+name="giga"
+print(f"hello {name}")
+
+#4) დაპტინტეთ ოთკუთხედი და ოთკუთხედის შუაში დაწერეთ რაიმე ტექსტი, მაგალითად:
+
+print("rom gaarchio")
+
+print("++++++++++++++")
+print("+ halloooooo +")
+print("++++++++++++++")
+
+#5) გააკეთეთ მარტივი მენიუ, ყველა საჭმელს მიუწერეთ ფასი (ყველა ახალ ხაზებზე გამოიტანეთ'''
+
+print("rom gaarchio")
+
+print("menu: ")
+print("lobiani : 10 lari")
+print("caesar salad : 20 lari")
+print("khinkali : 2 lari")
+print("adjarian khachapuri : 25 lari")
+print("mwvadi : 30 lari")
+print("bottle of water : 1.50 lari")
+print("coca-cola : 2 lari")
+
+#chemi versiaaaaaaa
+
+food1="lobiani"
+food2="caesar salad"
+food3="khinkali"
+food4="adjarian khachapuri"
+food5="mwvadi"
+drink1="bottle of water"
+drink2="coca-cola"
+
+price1=10 
+price2=20
+price3=2
+price4=25
+price5=30
+price6=1.50
+price7=2
+
+print("rom gaarchio")
+
+print(f"{food1} : {price1} lari")
+print(f"{food2} : {price2} lari")
+print(f"{food3} : {price3} lari")
+print(f"{food4} : {price4} lari")
+print(f"{food5} : {price5} lari")
+print(f"{drink1} : {price6} lari")
+print(f"{drink2} : {price7} lari")
+
+#f string tu gagikvirda naswavlimqondaa
