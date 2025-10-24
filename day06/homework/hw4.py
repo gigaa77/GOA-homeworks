ca1e08ce1a4e4094b0d4206977d9dafd08b9e5e0
gigaa77 <gogitidzegiga2009@gmail.com>
2025-10-24 21:51:40 +0400

    “commit”

---
+++ day06/homework/hw4.py
@@ -0,0 +1,11 @@
+#4) მოცემულია რიცხვი x. თუ რიცხვი ნაკლებია 10-ზე ან უდრის 0-ს, დაბეჭდე "პატარა ან ნულია", სხვა შემთხვევაში — "არ აკმაყოფილებს".
+
+
+
+x=10
+
+
+if x<10 or x==0:
+    print("x is little or is equal to 0")
+else:
+    print("it doesnt march the condition")
---
+++ day06/homework/hw5.py
@@ -0,0 +1,5 @@
+#5) რას გამოიტანს True and False or True or False and False and True or False and False
+
+print(True and False or True or False and False and True or False and False)
+
+# gamoitana true
