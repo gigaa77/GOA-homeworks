f7feb608fd6008f96abbc13ce3dd1aaa8a6df049
gigaa77 <gogitidzegiga2009@gmail.com>
2025-11-21 12:42:37 +0400

    “commit”

--- day14/homework/hw6.py
+++ day14/homework/hw6.py
@@ -4,10 +4,10 @@ number = input("enter a number or type 'calculate average' to calculate average:
 sum_numbers= 0 
 count_numbers=0
 
-while number != "გამოთვალე საშუალო":
+while number != "calculate average":
     count_numbers = count_numbers +1
     sum_numbers = sum_numbers + float(number)
-    number= input ("enter a number or type'გამოთვალე საშუალო' to calculate average: ")
+    number= input ("enter a number or type'calculate average' to calculate average: ")
 
 average= sum_numbers/count_numbers
 print(average)
