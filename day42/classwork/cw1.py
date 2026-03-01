c62994adedc494712312f81761415909bab453af
gigaa77 <gogitidzegiga2009@gmail.com>
2026-03-02 00:04:12 +0400

    “commit”

---
+++ day42/classwork/cw1.py
@@ -0,0 +1,42 @@
+#codewars 2
+# def count_sheeps(sheep):
+#     count = 0
+#     for i in count:
+#         if i == True:
+#             count +=1
+#     return count
+
+
+
+
+#codewars3
+# def summation(num):
+#     total = 0
+#     for i in range(1, i + 1):
+#         total +=i
+#     return total
+    
+
+
+#codewars4
+# def twice_as_old(d,s):
+#     age = d - 2 * s
+    
+#     if age <0:
+#         return -age
+#     else:
+#         return age
+
+# codewars5
+
+# def number(bus_stops):
+#     get_on = 0
+#     get_off = 0
+    
+#     for i in bus_stops:
+#         get_on += i[0]
+#         get_off +=i[1]
+    
+#     sum = get_on - get_off
+    
+#     return sum
